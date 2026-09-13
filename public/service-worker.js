f6499d5b9ad044731f02a43ac2407d31d2863faf
Henrique <henrique_roberto121@hotmail.com>
2026-09-12 22:25:50 -0300

    fix service worker POST requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -33,6 +33,8 @@ self.addEventListener('activate', event => {
     self.clients.claim();
 })
 self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+
     event.respondWith(
         fetch(event.request)
             .then(response =>{
